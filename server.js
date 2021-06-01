ef7937fb5240c2822a84083a0b34f9244928ad2f
Jacob Hildebrandt <jake@hildy.ca>
2021-06-01 15:54:18 -0700

    Setup data for Namespaces and Rooms. Listen for each namespace by looping through each Namespace

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const socketio = require("socket.io");
 const cors = require("cors");
 
+let namespaces = require("./data/namespaces");
+
 app.use(cors());
 
 const expressServer = app.listen(9000, () => {
@@ -14,20 +16,19 @@ const io = socketio(expressServer, {
 });
 
 io.on("connection", (socket) => {
-  socket.emit("messageFromServer", {
-    data: "Welcome to the socket.io server!",
-  });
+  socket.emit("messageFromServer", "Welcome to the socket.io server!");
   socket.on("messageToServer", (dataFromClient) => {
     console.log(dataFromClient);
   });
   socket.on("newMessageToServer", (msg) => {
-    console.log(msg);
-    io.emit("messageToClients", { text: msg.text });
     io.of("/").emit("messageToClients", { text: msg.text });
   });
 });
 
-io.of("/admin").on("connection", (socket) => {
-  console.log("someone connected to the admin namespace.");
-  io.of("/admin").emit("welcome", "Welcome to the admin channel");
+// loop through each namespace and listen for a connection
+namespaces.forEach((namespace) => {
+  //   console.log(namespace);
+  io.of(namespace.endpoint).on("connection", (socket) => {
+    console.log(`${socket.id} has joined ${namespace.endpoint}`);
+  });
 });
